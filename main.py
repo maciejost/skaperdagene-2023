7c85ca08e6ba97841b3a73a3e8c04fdd9121b308
Maciej Ostrowski <maciej.ostrowski@fremtind.no>
2023-10-25 15:28:52 +0200

    Init

---
+++ main.py
@@ -0,0 +1,60 @@
+import os
+import openai
+import random
+openai.api_key = "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"
+
+prompts = [
+	"Gi meg et kort vers på seks linjer som rimer om hvor kul en person er, personen heter ",
+	"Gi meg en haiku om hvor fantastisk en person er, personen heter ",
+]
+
+people = [
+	{
+		"name": "Maciej",
+		"rfid": "1234567890"
+	},
+	{
+		"name": "Ragna",
+		"rfid": "0987654321"
+	},
+	{
+		"name": "Sindre",
+		"rfid": "1234567890"
+	},
+	{
+		"name": "Mathias",
+		"rfid": "0987654321"
+	},
+	{
+		"name": "Karolina",
+		"rfid": "1234567890"
+	},
+	{
+		"name": "Torbjørn",
+		"rfid": "0987654321"
+	},
+	{
+		"name": "Harald",
+		"rfid": "1234567890"
+	}
+]
+
+def getName():
+	# Få svar fra "rfid"
+	# Sjekk om "rfid" er i people
+	# Hvis ja, returner navn
+	return "Robbin"
+
+
+def fetchGreeting(name):
+	prompt = random.choice(prompts) + name + "."
+	completion = openai.ChatCompletion.create(
+		model="gpt-3.5-turbo",
+		messages=[
+			{"role": "user", "content": prompt},
+		]
+	)
+	return completion.choices[0].message.content
+
+
+print(fetchGreeting(getName()))
